13ea634354e57d8766ab7d02372f03fdde59a4a7
Eroto <seyive.awaya@gmail.com>
2024-07-19 17:59:54 +0100

    adding header to requests

--- arrow.com/utils/parser.py
+++ arrow.com/utils/parser.py
@@ -3,11 +3,11 @@ import requests
 def requestHandler(url:str)->dict:
     
     try:
-        response = requests.get(url, timeout=10)
-
+        response = requests.get(url, timeout=60, headers={"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0", "Accept-Language": "en-US, fr-FR", "referrer": "https://www.arrow.com/", "connection": "keep-alive"  })
+        
         response.raise_for_status()        
         
-        return {"data":response.json()}
+        return {"data":response.json(), "content":response.content}
         
     except requests.exceptions.RequestException as e:
 
